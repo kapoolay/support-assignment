b6698ac32dadf05bfc77e1e496499af17182eb36
Kevin Capule <kapoolay@gmail.com>
2022-02-14 22:10:39 -0600

    main branch push

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,22 @@ import wget
 
 st.title("My Streamlit App!")
 
+# Choose/Submit Prompt
 with st.form("app_form"):
-  status_code = st.radio("Pick a status code", ('101','102','405','406','407','416','417','500','502','521'))
-  
+  status_code = st.radio("Pick a status code", ('101', '102', '405', '406', '407', '416', '417', '500', '502', '521'))
+
   submitted = st.form_submit_button("Submit")
   if submitted:
     st.write("You pressed submit!")
+
+
+# Get the response
+site_url = f"https://http.cat/{status_code}"
+
+# Display the results
+st.header("Results:")
+st.image(site_url, caption='Cat')
+
+
+# Celebrate
+st.balloons()
